13d903540f55651da68240426fd9b4174d273c47
Eric Sellors <github@esellors.com>
2021-06-29 13:51:28 -0500

    final

--- server.js
+++ server.js
@@ -9,17 +9,46 @@ const rollbar = new Rollbar({
 })
 
 app.use(express.json())
+app.use('/style', express.static('./public/styles.css'))
 
 
-app.get('/', function(req, res) {
-    rollbar.log('Hello World')
+
+let students = [] // we'll hold any students added here
+
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file served successfully')
 })
 
+app.post('/api/student', (req, res) => {
+    let {name} = req.body
+    name = name.trim()
 
+    const index = students.findIndex((studentName) => { // check if student name exists already
+        studentName === name
+    })
 
-const port = process.env.PORT || 4545;
-
-app.listen(port, function() { 
-    console.log(`Server jamming on ${port}`)
+    try { // using a "try catch" block will handle any generic 500 errors (not necessary, but a good addition)
+        if (index === -1 && name !== '') {
+            // we'll send responses to the user based upon whether or not they gave us a valid user to add
+            // also we'll send information to rollbar so we can keep track of the activity that's happening
+            students.push(name)
+            rollbar.log('student added successfully', {author: 'riley', type: 'manual'})
+            res.status(200).send(students)
+        } else if (name === '') {
+            rollbar.error('no name given')
+            res.status(400).send('must provide a name')
+        } else {
+            rollbar.error('student already exists')
+            res.status(400).send('that student already exists')
+        }
+    } catch (err) {
+        rollbar.error(err)
+    }
 })
+
+const port = process.env.PORT || 4545
+
+app.use(rollbar.errorHandler()) // // Use the rollbar error handler to send exceptions to your rollbar account for logging
+
+app.listen(port, () => console.log(`running on port: ${port}`))
