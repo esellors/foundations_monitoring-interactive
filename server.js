fb5f8b9e4e7809f9138d2a4ea64704a67cd2eb73
Eric Sellors <github@esellors.com>
2021-06-29 13:30:22 -0500

    added APM

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: '7bca008bb8a24b7a8046eba88cde042c',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
 app.use(express.json())
 
 
 app.get('/', function(req, res) {
+    rollbar.log('Hello World')
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
